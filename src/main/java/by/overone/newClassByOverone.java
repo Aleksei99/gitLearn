dd9809e56243d938f6c7ad1d6efc2c6849457bed
Aleksei <smurahaleha@gmail.com>
2021-11-02 15:12:08 +0300

    alover to pull req

--- src/main/java/by/overone/newClassByOverone.java
+++ src/main/java/by/overone/newClassByOverone.java
@@ -13,5 +13,7 @@ public class newClassByOverone {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+
+        System.out.println("to pull req from alOverone");
     }
 }
