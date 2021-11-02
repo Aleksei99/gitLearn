a8c4fce86467b5d61a397e7181b6e738d4542022
Aleksei <smurahaleha@gmail.com>
2021-11-02 15:16:33 +0300

    alover to pull req2 true

--- src/main/java/by/overone/newClassByOverone.java
+++ src/main/java/by/overone/newClassByOverone.java
@@ -9,5 +9,7 @@ public class newClassByOverone {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+
+        System.out.println("Великий Гэтсби");
     }
 }
