1eb7d09f1e9b5c3bca41248922a05971ccc3de22
Aleksei <smurahaleha@gmail.com>
2021-11-02 14:34:25 +0300

    added new class

---
+++ src/main/java/by/overone/newClassByOverone.java
@@ -0,0 +1,9 @@
+package by.overone;
+
+public class newClassByOverone {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
